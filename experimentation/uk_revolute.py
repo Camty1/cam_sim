4bae07b376b6d5f418f48fde0963729e725de7dd
Cameron Wolfe <cameron.wolfe@spacex.com>
2026-07-01 18:59:58 -0700

    unconstrained dynamics

--- experimentation/uk_revolute.py
+++ experimentation/uk_revolute.py
@@ -105,6 +105,16 @@ class RigidbodyKinematics:
 
 
 @dataclass
+class RigidbodyDerivedMassProperties:
+    """
+    Mass properties that do not change over time
+    """
+
+    initialized: bool = False
+    inertia_inv_body_wrt_cm_in_body: np.ndarray = np.zeros((3, 3))
+
+
+@dataclass
 class Rigidbody:
     """
     A single rigidbody
@@ -117,6 +127,9 @@ class Rigidbody:
     v_body_wrt_0_in_0: np.ndarray
     sigma_dot_0_to_body: np.ndarray
     body_num: int
+    derived_mass_properties: RigidbodyDerivedMassProperties = field(
+        default_factory=lambda: RigidbodyDerivedMassProperties()
+    )
     kinematics: RigidbodyKinematics = field(
         default_factory=lambda: RigidbodyKinematics()
     )
@@ -129,8 +142,24 @@ class Rigidbody:
         assert self.v_body_wrt_0_in_0.shape == (3, 1)
         assert self.sigma_dot_0_to_body.shape == (3, 1)
         assert self.body_num >= 0
+        self.calculate_derived_mass_properties()
+
+    def initialize_derived_mass_properties(self) -> None:
+        """
+        Initialize derived mass properties (should be called once at the beginning of the function)
+        """
+        assert not self.derived_mass_properties.initialized
+
+        self.derived_mass_properties.inertia_inv_body_wrt_cm_in_body = np.linalg.inv(
+            self.inertia_body_wrt_cm_in_body
+        )
+
+        self.derived_mass_properties.initialized = True
 
     def populate_kinematics(self) -> None:
+        """
+        Populates the kinematics (should be called each timestep)
+        """
         self.kinematics.c_0_to_body = mrp_to_dcm(self.sigma_0_to_body)
         self.kinematics.c_body_to_0 = mrp_to_dcm(-self.sigma_0_to_body)
         self.kinematics.b = mrp_b_matrix(self.sigma_0_to_body)
@@ -150,6 +179,37 @@ class Rigidbody:
         self.kinematics.tilde_omega_body_wrt_0_in_body_squared = (
             self.kinematics.tilde_omega_body_wrt_0_in_body
             @ self.kinematics.tilde_omega_body_wrt_0_in_body
+        )
+
+    def unconstrained_dynamics(
+        self,
+        force_body_wrt_0_in_0: np.ndarray = np.zeros((3, 1)),
+        moment_body_wrt_0_in_body: np.ndarray = np.zeros((3, 1)),
+    ) -> np.ndarray:
+        """
+        Calculates the unconstrained dynamics of the rigidbody
+        """
+        assert self.derived_mass_properties.initialized
+        assert force_body_wrt_0_in_0.shape == (3, 1)
+        assert moment_body_wrt_0_in_body.shape == (3, 1)
+
+        scaled_r_ddot_body_wrt_0_in_0 = force_body_wrt_0_in_0
+        scaled_sigma_ddot_0_to_body = (
+            self.kinematics.b_dot @ self.kinematics.omega_body_wrt_0_in_body
+            + self.kinematics.b
+            @ (
+                self.derived_mass_properties.inertia_inv_body_wrt_cm_in_body
+                @ (
+                    moment_body_wrt_0_in_body
+                    - self.kinematics.tilde_omega_body_wrt_0_in_body
+                    @ self.inertia_body_wrt_cm_in_body
+                    @ self.kinematics.omega_body_wrt_0_in_body
+                )
+            )
+        )
+
+        return np.stack(
+            [scaled_r_ddot_body_wrt_0_in_0, scaled_sigma_ddot_0_to_body], axis=0
         )
 
 
@@ -199,12 +259,15 @@ class RevoluteJoint:
         assert self.joint_num >= 0
 
     def populate_kinematics(self, body1: Rigidbody, body2: Rigidbody) -> None:
+        """
+        Populates the kinematics of the joint (called at the start of each timestep)
+        """
         self.kinematics.tilde_r_joint_wrt_1_in_1 = tilde(self.r_joint_wrt_1_in_1)
         self.kinematics.tilde_r_joint_wrt_2_in_2 = tilde(self.r_joint_wrt_2_in_2)
         self.kinematics.tilde_joint_direction_in_1 = tilde(self.joint_direction_in_1)
         self.kinematics.tilde_joint_direction_in_2 = tilde(self.joint_direction_in_2)
-        self.kinematics.body1_joint_direction_in_0 = body1.kinematics.c_body_to_0
-        self.kinematics.body2_joint_direction_in_0 = body2.kinematics.c_body_to_0
+        self.kinematics.body1_joint_direction_in_0 = body1.kinematics.c_body_to_0 @ self.joint_direction_in_1
+        self.kinematics.body2_joint_direction_in_0 = body2.kinematics.c_body_to_0 @ self.joint_direction_in_2
         self.kinematics.tilde_body1_joint_direction_in_0 = tilde(
             self.kinematics.body1_joint_direction_in_0
         )
@@ -224,6 +287,19 @@ class RevoluteJoint:
 
 
 @dataclass
+class MultiRigidbodyDerivedMassProperties:
+    """
+    Derived mass properties for a MultiRigidbody.  Must be initialized after adding all bodies
+    """
+
+    initialized: bool = False
+    mass_matrix: np.ndarray = np.zeros(0)
+    mass_matrix_inv: np.ndarray = np.zeros(0)
+    mass_matrix_sqrt: np.ndarray = np.zeros(0)
+    mass_matrix_inv_sqrt: np.ndarray = np.zeros(0)
+
+
+@dataclass
 class MultiRigidbody:
     """
     A collection of bodies joined by joints
@@ -231,6 +307,9 @@ class MultiRigidbody:
 
     bodies: list[Rigidbody] = field(default_factory=list)
     joints: list[RevoluteJoint] = field(default_factory=list)
+    derived_mass_properties: MultiRigidbodyDerivedMassProperties = field(
+        default_factory=lambda: MultiRigidbodyDerivedMassProperties()
+    )
 
     def add_body(
         self,
@@ -241,6 +320,10 @@ class MultiRigidbody:
         v_body_wrt_0_in_0: np.ndarray = np.zeros((3, 1)),
         sigma_dot_0_to_body: np.ndarray = np.zeros((3, 1)),
     ) -> None:
+        """
+        Adds a rigidbody
+        """
+        assert not self.derived_mass_properties.initialized
         body_num = len(self.bodies)
         self.bodies.append(
             Rigidbody(
@@ -263,11 +346,15 @@ class MultiRigidbody:
         joint_direction_in_1: np.ndarray = np.array([[0], [0], [1]]),
         joint_direction_in_2: np.ndarray = np.array([[0], [0], [1]]),
     ) -> None:
+        """
+        Adds a revolute joint
+        """
         assert body1_idx >= 0
         assert body1_idx < len(self.bodies)
         assert body2_idx >= 0
         assert body2_idx < len(self.bodies)
         assert body1_idx != body2_idx
+        assert not self.derived_mass_properties.initialized
         joint_num = len(self.joints)
         self.joints.append(
             RevoluteJoint(
@@ -281,7 +368,50 @@ class MultiRigidbody:
             )
         )
 
+    def initialize_derived_mass_properties(self) -> None:
+        """
+        Initializes derived mass properties which are used in dynamics calculations
+        """
+        assert not self.derived_mass_properties.initialized
+        self.derived_mass_properties.mass_matrix = np.zeros(
+            (6 * len(self.bodies), 6 * len(self.bodies))
+        )
+        for body_idx, body in enumerate(self.bodies):
+            body.
+            start_idx = 6 * body_idx
+            end_idx = 6 * (body_idx + 1)
+            self.derived_mass_properties.mass_matrix[
+                start_idx:end_idx, start_idx:end_idx
+            ] = np.diag([body.m_body, body.m_body, body.m_body, 4, 4, 4])
+            self.derived_mass_properties.mass_matrix_inv[
+                start_idx:end_idx, start_idx:end_idx
+            ] = np.diag(
+                [1 / body.m_body, 1 / body.m_body, 1 / body.m_body, 1 / 4, 1 / 4, 1 / 4]
+            )
+            self.derived_mass_properties.mass_matrix_sqrt[
+                start_idx:end_idx, start_idx:end_idx
+            ] = np.sqrt(np.diag([body.m_body, body.m_body, body.m_body, 4, 4, 4]))
+            self.derived_mass_properties.mass_matrix_inv_sqrt[
+                start_idx:end_idx, start_idx:end_idx
+            ] = np.sqrt(
+                np.diag(
+                    [
+                        1 / body.m_body,
+                        1 / body.m_body,
+                        1 / body.m_body,
+                        1 / 4,
+                        1 / 4,
+                        1 / 4,
+                    ]
+                )
+            )
+
+        self.derived_mass_properties.initialized = True
+
     def state(self) -> np.ndarray:
+        """
+        Returns x
+        """
         body_states: list[np.ndarray] = []
         for body in self.bodies:
             body_states.append(body.r_body_wrt_0_in_0)
@@ -290,6 +420,9 @@ class MultiRigidbody:
         return np.stack(body_states)
 
     def state_dot(self) -> np.ndarray:
+        """
+        Returns x_dot
+        """
         body_state_dots: list[np.ndarray] = []
         for body in self.bodies:
             body_state_dots.append(body.v_body_wrt_0_in_0)
@@ -298,6 +431,9 @@ class MultiRigidbody:
         return np.stack(body_state_dots)
 
     def populate_kinematics(self) -> None:
+        """
+        Called at the beginning of each timestep, populates the derived kinematics
+        """
         for body in self.bodies:
             body.populate_kinematics()
 
@@ -306,7 +442,20 @@ class MultiRigidbody:
                 self.bodies[joint.body1_idx], self.bodies[joint.body2_idx]
             )
 
+    def unconstrained_dynamics(self) -> np.ndarray:
+        """
+        Loops through bodies to get M @ x_ddot
+        """
+        body_scaled_state_ddots: list[np.ndarray] = []
+        for body in self.bodies:
+            body_scaled_state_ddots.append(body.unconstrained_dynamics())
+
+        return np.stack(body_scaled_state_ddots, axis=0)
+
     def uk_a_matrix_and_b_vector(self) -> tuple[np.ndarray, np.ndarray]:
+        """
+        Gets the UK A matrix and b vector from looping through constraints
+        """
         state_dim = len(self.bodies) * 6
         n_constraint_eqns = len(self.joints) * 6
 
@@ -404,5 +553,23 @@ class MultiRigidbody:
         return a_matrix, b_vector
 
     def uk_dynamics(self) -> np.ndarray:
+        """
+        Gets x_ddot taking into account the unconstrained equations of motion + constraints
+
+        x_ddot = M^{-1}q + M^{-1} A^T (A M^{-1} A^T)^{-1} (b - A M^{-1} q)
+        """
+        if not self.derived_mass_properties.initialized:
+            raise ValueError("Derived mass properties have not been initialized")
         self.populate_kinematics()
-        unconstrained_state_ddot = self.unconstrained_dynamics()
+        q = self.unconstrained_dynamics()
+        a_matrix, b_vector = self.uk_a_matrix_and_b_vector()
+
+        # Precompute common terms
+        mass_matrix_inv_q = self.derived_mass_properties.mass_matrix_inv @ q
+        a_matrix_mass_matrix_inv = a_matrix @ self.derived_mass_properties.mass_matrix_inv
+
+        # Solve UK dynamics
+        lambda_vec = np.linalg.solve(a_matrix_mass_matrix_inv @ a_matrix.T, b_vector - a_matrix_mass_matrix_inv @ q)
+        x_ddot = mass_matrix_inv_q + self.derived_mass_properties.mass_matrix_inv @ a_matrix.T @ lambda_vec
+
+        return x_ddot
